27a4558b962f421ab146ec48844b63964bc58bb4
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-11-28 09:27:59 +0000

    Add project files.

---
+++ GaryGillespie_Practical/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GaryGillespie_Practical.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
